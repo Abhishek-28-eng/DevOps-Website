bc180699e0ca7ebd4a0ae00c022068ccdfed156c
dgschooldevelopment <Dreamsguider@gmail.com>
2025-11-14 23:08:01 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api";
+const base = import.meta.env.VITE_API_BASE_URL || "http://devwithabhi.de/api";
 
 const instance = axios.create({
   baseURL: base,
